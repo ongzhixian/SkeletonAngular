779ccde64f917d057c03b2ec34a5f65dd7007566
Zhixian Ong <zhixian.ong@ti8m.ch>
2022-09-27 18:00:37 +0800

    update

--- skeleton-app/src/app/services/portfolio.service.ts
+++ skeleton-app/src/app/services/portfolio.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { Portfolio } from '../models/portfolio';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class PortfolioService {
 
-  constructor() { }
+    constructor() { }
 
-  getPortfolioList() : Portfolio[] {
-    return [
-      {id : "pf1", name : "pf1 name", description : "desc1"},
-      {id : "pf2", name : "pf2 name", description : "desc2"},
-      {id : "pf3", name : "pf3 name", description : "desc3"},
-    ];
-  }
+    private selectedPortfolio = new Subject<Portfolio>();
+
+    portfolioSelected$ = this.selectedPortfolio.asObservable();
+
+    getPortfolioList(): Portfolio[] {
+        return [
+            { id: "pf1", name: "pf1 name", description: "desc1" },
+            { id: "pf2", name: "pf2 name", description: "desc2" },
+            { id: "pf3", name: "pf3 name", description: "desc3" },
+        ];
+    }
+
+    selectPortfolio(portfolio: Portfolio) {
+        this.selectedPortfolio.next(portfolio);
+    }
 }
